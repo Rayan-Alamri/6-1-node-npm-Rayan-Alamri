1c0f258ced2263ee5dadf628e4a4ffa942ae1063
Rayan-Alamri <s202247900@kfupm.edu.sa>
2025-11-11 21:53:13 +0300

    submission

--- utils/parser.js
+++ utils/parser.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-  return _.map(input.split(","), (num) => parseFloat(num.trim()));
+  const wrapped = _.map(input, (value) => {
+    const parsed = Number(value);
+    return Number.isFinite(parsed) ? { value: parsed } : null;
+  });
+
+  return _.map(_.compact(wrapped), "value");
 }
 
 export function isValidOperation(operation) {
-  return ["add", "subtract", "multiply", "divide"].includes(operation);
+  const validOps = ["add", "subtract", "multiply", "divide"];
+  return _.includes(validOps, operation);
 }
 
